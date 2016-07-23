c6d2041532ba6ce3c0036b2337b4cde462af987b
student <student>
2016-07-23 15:30:25 +0900

    なんで固定番号使う

--- 固定番号認証/u_get_1.php
+++ 固定番号認証/u_get_1.php
@@ -4,6 +4,8 @@
 header("Content-Type: application/json; charset=utf-8");
 
 // uuidを作成
+//uuidを使うメリットは毎回ログイン時に、idとpasswordを入力せずに、ログインできる//個人の携帯ゲームなどに向いている
+
 $uuid = strtolower(trim(`uuidgen -r`));
 
 //
